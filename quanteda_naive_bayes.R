678651bd234f00636ac6a523bcbf9ec1cfc598e5
Soteriades Andreas - Data Scientist <andreassot10@gmail.com>
2020-09-21 13:05:01 +0100

    Multinomial Naive Bayes model with Quanteda on simple train/test split (no cross-validation etc.). Accuracy 60-63%.

--- quanteda_naive_bayes.R
+++ quanteda_naive_bayes.R
@@ -1,23 +1,75 @@
+library(tidyverse)
 library(quanteda)
 library(quanteda.textmodels)
-source('mlr3_load_packages.R')
+library(caret) # Function confusionMatrix()
 
-# Load data and prepare training and test sets
-source('mlr3_data_clean_and_prepare.R')
-source('mlr3_prepare_test_and_training_tasks.R')
-# The loaded data frame is called pipeline_data
+# Train and test text data with Quanteda's multinomial Naive Bayes
+# Predictor is variable 'improve' and response is variable 'super'
+# See https://tutorials.quanteda.io/machine-learning/nb/
 
-pipeline_data_dfm <- dfm(pipeline_data$improve)
+# Load, clean and prepare data
+load('cleanData.RData')
 
-nb <- textmodel_nb(
-  x = pipeline_data_dfm[train.idx, ], 
-  y = pipeline_data$super[test.idx], 
-  prior = 'termfreq')
-
-preds <- 
-  data.frame(
-    predicted = predict(nb, newdata = pipeline_data_dfm[test.idx, ])
+quanteda_data <- trustData %>%
+  mutate_if(is.factor, as.character) %>%
+  mutate_if(is.Date, as.POSIXct) %>%
+  clean_names %>%
+  left_join(
+    select(categoriesTable, Number, Super), 
+    by = c('imp1' = 'Number')
   ) %>%
-  mutate(true = pipeline_data$super[test.idx])
+  clean_names %>%
+  select(super, date, division2, directorate2, improve) %>%
+  filter_all(~ !is.na(.)) %>%
+  filter(
+    !super %in% c('Equality/Diversity', 
+      'Physical Health', 'Record Keeping', 'Safety', 'MHA', 'Smoking', 'Leave')
+  ) %>% # Too few in the data (e.g. 2-3 in 10-30% data samples)
+  as_tibble
 
-sum(preds$true == preds$predicted) / nrow(preds)
+names(quanteda_data)
+
+# Create a corpus that also has a doc ID and a dependent variable
+corpus_patient_feedback <- corpus(quanteda_data$improve)
+corpus_patient_feedback$id_numeric <- 1:ndoc(corpus_patient_feedback)
+corpus_patient_feedback$super <- quanteda_data$super
+summary(corpus_patient_feedback, 5)
+
+# Random sample without replacement for training
+id_train <- sample(
+  1:length(corpus_patient_feedback), 
+  round(2 / 3 * length(corpus_patient_feedback)), 
+  replace = FALSE
+)
+
+# Training set
+quanteda_data_training <- corpus_subset(
+  corpus_patient_feedback, 
+  id_numeric %in% id_train
+) %>%
+  dfm(remove = stopwords("english"), stem = TRUE)
+
+# Test set
+quanteda_data_test <- corpus_subset(
+  corpus_patient_feedback, 
+  !id_numeric %in% id_train
+) %>%
+  dfm(remove = stopwords("english"), stem = TRUE)
+
+# Naive Bayes
+quanteda_nb <- textmodel_nb(quanteda_data_training, 
+  quanteda_data_training$super, prior = 'termfreq')
+summary(quanteda_nb)
+
+# With Naive Bayes, features must occur both in the training set and the test sets
+# Make the features identical
+quanteda_data_matched <- dfm_match(quanteda_data_test, 
+  features = featnames(quanteda_data_training))
+
+# Confusion matrix and metrics
+actual_class <- quanteda_data_matched$super
+predicted_class <- predict(quanteda_nb, newdata = quanteda_data_matched)
+tab_class <- table(actual_class, predicted_class)
+tab_class
+
+confusionMatrix(tab_class, mode = "everything")
