bf4e25c73427c70e0e25f97b935233c9c9d7536f
andreassot10 <61313624+andreassot10@users.noreply.github.com>
2020-10-07 13:53:28 +0100

    Benchmark learners
    Python pipeline benchmarking learners. Elementary parameter tuning.

--- text_mining.py
+++ text_mining.py
@@ -1,34 +1,33 @@
 """
-======================================================
-Classification of text documents using sparse features
-======================================================
+================================================================================
+Classification of patient feedback using algoritmhs that can efficiently
+handle sparse matrices
+================================================================================
 
-This is an example showing how scikit-learn can be used to classify documents
-by topics using a bag-of-words approach. This example uses a scipy.sparse
-matrix to store the features and demonstrates various classifiers that can
-efficiently handle sparse matrices.
+Classify documents by label using a bag-of-words approach.
+
+See example in https://scikit-learn.org/stable/auto_examples/text/plot_document_classification_20newsgroups.html?highlight=text%20classification%20sparse
 
 """
 
-# Code adopted from:
-# https://scikit-learn.org/stable/auto_examples/text/plot_document_classification_20newsgroups.html?highlight=text%20classification%20sparse
 
 import logging
 import numpy as np
 import pandas as pd
+from pandas import DataFrame
 from optparse import OptionParser
 import sys
 from time import time
 import matplotlib.pyplot as plt
 
-from pandas import DataFrame
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.feature_selection import SelectFromModel
 from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.linear_model import LogisticRegression
 from sklearn.linear_model import RidgeClassifier
-from sklearn.pipeline import Pipeline
 from sklearn.svm import LinearSVC
 from sklearn.linear_model import SGDClassifier
 from sklearn.linear_model import Perceptron
@@ -39,127 +38,47 @@ from sklearn.neighbors import NearestCentroid
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.utils.extmath import density
 from sklearn import metrics
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import f1_score
+from sklearn.model_selection import GridSearchCV
 
-
-# Display progress logs on stdout
-logging.basicConfig(level=logging.INFO,
-                    format='%(asctime)s %(levelname)s %(message)s')
-
-op = OptionParser()
-op.add_option("--report",
-              action="store_true", dest="print_report",
-              help="Print a detailed classification report.")
-op.add_option("--chi2_select",
-              action="store", type="int", dest="select_chi2",
-              help="Select some number of features using a chi-squared test")
-op.add_option("--confusion_matrix",
-              action="store_true", dest="print_cm",
-              help="Print the confusion matrix.")
-op.add_option("--top10",
-              action="store_true", dest="print_top10",
-              help="Print ten most discriminative terms per class"
-                   " for every classifier.")
-op.add_option("--all_categories",
-              action="store_true", dest="all_categories",
-              help="Whether to use all categories or not.")
-op.add_option("--use_hashing",
-              action="store_true",
-              help="Use a hashing vectorizer.")
-op.add_option("--n_features",
-              action="store", type=int, default=2 ** 16,
-              help="n_features when using the hashing vectorizer.")
-op.add_option("--filtered",
-              action="store_true",
-              help="Remove newsgroup information that is easily overfit: "
-                   "headers, signatures, and quoting.")
-
-
-def is_interactive():
-    return not hasattr(sys.modules['__main__'], '__file__')
-
-
-# work-around for Jupyter notebook and IPython console
-argv = [] if is_interactive() else sys.argv[1:]
-(opts, args) = op.parse_args(argv)
-if len(args) > 0:
-    op.error("this script takes no arguments.")
-    sys.exit(1)
-
-print(__doc__)
-op.print_help()
-print()
+RandomForestClassifier().get_params()
+MultinomialNB().get_params()
 
 
 ##############################################################################
 # Load data from the training set
 # ------------------------------------
-filename = "/Users/afsa43/git_projects/positive_about_change_text_mining/text_data.csv"
+filename = "text_data.csv"
 text_data = pd.read_csv(filename)
 text_data = text_data.rename(columns={'super': 'target'})
-text_data.head()
-
-# order of labels in `target_names` can be different from `categories`
-target_names = text_data.target # MONITOR THIS. I took it from the example script but don't know what it does. Should it be defined on the whole dataset or train data only?
+type(text_data)
 
 # split a training set and a test set
-data_train, data_test = train_test_split(
-    text_data, test_size=0.33, random_state=42, stratify=text_data.target)
+X = text_data['improve']  # This way it's a series. Don't do text_data.drop...
+y = text_data['target'].to_numpy()
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                    test_size=0.33,
+                                                    stratify=y,
+                                                    shuffle=True,
+                                                    random_state=42
+                                                    )
 
-y_train = data_train.target
-y_test = data_test.target
-
-print("Extracting features from the training data using a sparse vectorizer")
-t0 = time()
-if opts.use_hashing:
-    vectorizer = HashingVectorizer(stop_words='english', alternate_sign=False)
-    X_train = vectorizer.transform(data_train['improve'].values.tolist())
-else:
-    vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5,
-                                 stop_words='english')
-    X_train = vectorizer.fit_transform(data_train['improve'].values.tolist())
-duration = time() - t0
-# print("done in %fs at %0.3fMB/s" % (duration, data_train_size_mb / duration))
-print("n_samples: %d, n_features: %d" % X_train.shape)
-print()
-
-print("Extracting features from the test data using the same vectorizer")
-t0 = time()
-X_test = vectorizer.transform(data_test['improve'].values.tolist())
-duration = time() - t0
-# print("done in %fs at %0.3fMB/s" % (duration, data_test_size_mb / duration))
-print("n_samples: %d, n_features: %d" % X_test.shape)
-print()
-
-# mapping from integer feature name to original token string
-if opts.use_hashing:
-    feature_names = None
-else:
-    feature_names = vectorizer.get_feature_names()
-
-if opts.select_chi2:
-    print("Extracting %d best features by a chi-squared test" %
-          opts.select_chi2)
-    t0 = time()
-    ch2 = SelectKBest(chi2, k=opts.select_chi2)
-    X_train = ch2.fit_transform(X_train, y_train)
-    X_test = ch2.transform(X_test)
-    if feature_names:
-        # keep selected feature names
-        feature_names = [feature_names[i] for i
-                         in ch2.get_support(indices=True)]
-    print("done in %fs" % (time() - t0))
-    print()
-
-if feature_names:
-    feature_names = np.asarray(feature_names)
+learners = [RidgeClassifier(),
+            LinearSVC(),
+            SGDClassifier(),
+            Perceptron(),
+            PassiveAggressiveClassifier(),
+            BernoulliNB(),
+            ComplementNB(),
+            MultinomialNB(),
+            KNeighborsClassifier(),
+            NearestCentroid(),
+            # RandomForestClassifier()
+            ]
 
 
-def trim(s):
-    """Trim string to fit on terminal (assuming 80-column display)"""
-    return s if len(s) <= 80 else s[:77] + "..."
-
-
-##############################################################################
+#############################################################################
 # Benchmark classifiers
 # ------------------------------------
 # We train and test the datasets with 15 different classification models
@@ -169,37 +88,27 @@ def benchmark(clf):
     print("Training: ")
     print(clf)
     t0 = time()
-    clf.fit(X_train, y_train)
+    pipe = Pipeline([
+        ('vect', CountVectorizer()),
+        ('tfidf', TfidfTransformer()),
+        ('kbest', SelectKBest(chi2, k=10000)),
+        ('clf', clf)
+    ])
+    param_grid = [
+        {'vect__ngram_range': [(1, 1), (2, 2)]}
+    ]
+    gs = GridSearchCV(pipe, param_grid)
+    gs.fit(X_train, y_train)
     train_time = time() - t0
     print("train time: %0.3fs" % train_time)
 
     t0 = time()
-    pred = clf.predict(X_test)
+    pred = gs.predict(X_test)
     test_time = time() - t0
     print("test time:  %0.3fs" % test_time)
 
     score = metrics.accuracy_score(y_test, pred)
     print("accuracy:   %0.3f" % score)
-
-    if hasattr(clf, 'coef_'):
-        print("dimensionality: %d" % clf.coef_.shape[1])
-        print("density: %f" % density(clf.coef_))
-
-        if opts.print_top10 and feature_names is not None:
-            print("top 10 keywords per class:")
-            for i, label in enumerate(target_names):
-                top10 = np.argsort(clf.coef_[i])[-10:]
-                print(trim("%s: %s" % (label, " ".join(feature_names[top10]))))
-        print()
-
-    if opts.print_report:
-        print("classification report:")
-        print(metrics.classification_report(y_test, pred,
-                                            target_names=target_names))
-
-    if opts.print_cm:
-        print("confusion matrix:")
-        print(metrics.confusion_matrix(y_test, pred))
 
     print()
     clf_descr = str(clf).split('(')[0]
@@ -207,55 +116,8 @@ def benchmark(clf):
 
 
 results = []
-for clf, name in (
-        (RidgeClassifier(tol=1e-2, solver="sag"), "Ridge Classifier"),
-        (Perceptron(max_iter=50), "Perceptron"),
-        (PassiveAggressiveClassifier(max_iter=50),
-         "Passive-Aggressive"),
-        (KNeighborsClassifier(n_neighbors=10), "kNN"),
-        (RandomForestClassifier(), "Random forest")):
-    print('=' * 80)
-    print(name)
+for clf in learners:
     results.append(benchmark(clf))
-
-for penalty in ["l2", "l1"]:
-    print('=' * 80)
-    print("%s penalty" % penalty.upper())
-    # Train Liblinear model
-    results.append(benchmark(LinearSVC(penalty=penalty, dual=False,
-                                       tol=1e-3)))
-
-    # Train SGD model
-    results.append(benchmark(SGDClassifier(alpha=.0001, max_iter=50,
-                                           penalty=penalty)))
-
-# Train SGD with Elastic Net penalty
-print('=' * 80)
-print("Elastic-Net penalty")
-results.append(benchmark(SGDClassifier(alpha=.0001, max_iter=50,
-                                       penalty="elasticnet")))
-
-# Train NearestCentroid without threshold
-print('=' * 80)
-print("NearestCentroid (aka Rocchio classifier)")
-results.append(benchmark(NearestCentroid()))
-
-# Train sparse Naive Bayes classifiers
-print('=' * 80)
-print("Naive Bayes")
-results.append(benchmark(MultinomialNB(alpha=.01)))
-results.append(benchmark(BernoulliNB(alpha=.01)))
-results.append(benchmark(ComplementNB(alpha=.1)))
-
-print('=' * 80)
-print("LinearSVC with L1-based feature selection")
-# The smaller C, the stronger the regularization.
-# The more regularization, the more sparsity.
-results.append(benchmark(Pipeline([
-  ('feature_selection', SelectFromModel(LinearSVC(penalty="l1", dual=False,
-                                                  tol=1e-3))),
-  ('classification', LinearSVC(penalty="l2"))])))
-
 
 ##############################################################################
 # Add plots
