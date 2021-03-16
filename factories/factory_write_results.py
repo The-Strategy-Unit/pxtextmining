86cd95243c67f5b22b7b342ff4dc4d2256b8abd9
Andreas <andreassot10@gmail.com>
2021-03-16 15:46:19 +0000

    code: Change names of results to be saved.

--- factories/factory_write_results.py
+++ factories/factory_write_results.py
@@ -40,26 +40,27 @@ def factory_write_results(pipe, tuning_results, pred, accuracy_per_class, p_comp
     # Write results to database
     print("Writing to database...")
     if "tuning results" in objects_to_save:
-        tuning_results.to_sql(name="tuning_results", con=engine, if_exists="replace", index=False)
+        tuning_results.to_sql(name="tuning_results_" + target, con=engine, if_exists="replace", index=False)
 
     index_training_data = x_train.index
     if "index - training data" in objects_to_save:
-        pd.DataFrame(index_training_data).to_sql(name="index_training_data", con=engine,
+        pd.DataFrame(index_training_data).to_sql(name="index_training_data_" + target, con=engine,
                                                  if_exists="replace", index=False)
     
     index_test_data = x_test.index
     if "index - test data" in objects_to_save:
-        pd.DataFrame(index_test_data).to_sql(name="index_test_data", con=engine, if_exists="replace", index=False)
+        pd.DataFrame(index_test_data).to_sql(name="index_test_data_" + target, con=engine,
+                                             if_exists="replace", index=False)
 
     pred = pd.DataFrame(pred, columns=[target + "_pred"])
     pred["row_index"] = index_test_data
     # aux = pd.DataFrame(index_training_data, columns=["row_index"])
     # pred = pd.concat([pred.reset_index(drop=True), aux], axis=0)
     if "predictions" in objects_to_save:
-        pd.DataFrame(pred).to_sql(name="predictions", con=engine, if_exists="replace", index=False)
+        pd.DataFrame(pred).to_sql(name="predictions_test_" + target, con=engine, if_exists="replace", index=False)
 
     if "accuracy per class" in objects_to_save:
-        accuracy_per_class.to_sql(name="accuracy_per_class", con=engine, if_exists="replace", index=False)
+        accuracy_per_class.to_sql(name="accuracy_per_class_" + target, con=engine, if_exists="replace", index=False)
 
     # Write results to disk
     if objects_to_save:
@@ -72,7 +73,7 @@ def factory_write_results(pipe, tuning_results, pred, accuracy_per_class, p_comp
 
     if "pipeline" in objects_to_save:
         if save_pipeline_as == "default":
-            aux = "finalized_model_" + ".sav"
+            aux = "finalized_model_" + target + ".sav"
             save_pipeline_as = path.join(results_file, aux)
         else:
             aux = save_pipeline_as + ".sav"
@@ -80,37 +81,37 @@ def factory_write_results(pipe, tuning_results, pred, accuracy_per_class, p_comp
         pickle.dump(pipe, open(save_pipeline_as, "wb"))
 
     if "tuning results" in objects_to_save and save_objects_to_disk:
-        aux = path.join(results_file, "tuning_results.csv")
+        aux = path.join(results_file, "tuning_results_" + target + ".csv")
         tuning_results.to_csv(aux, index=False)
         # aux = path.join(results_file, "tuning_results")
         # feather.write_dataframe(tuning_results, aux)
 
     if "predictions" in objects_to_save and save_objects_to_disk:
-        aux = path.join(results_file, "predictions.csv")
+        aux = path.join(results_file, "predictions_" + target + ".csv")
         pred.to_csv(aux, index=False)
         # aux = path.join(results_file, "predictions")
         # feather.write_dataframe(pd.DataFrame(pred), aux)
 
     if "accuracy per class" in objects_to_save and save_objects_to_disk:
-        aux = path.join(results_file, "accuracy_per_class.csv")
+        aux = path.join(results_file, "accuracy_per_class_" + target + ".csv")
         accuracy_per_class.to_csv(aux, index=False)
         # aux = path.join(results_file, "accuracy_per_class")
         # feather.write_dataframe(accuracy_per_class, aux)
 
     if "index - training data" in objects_to_save and save_objects_to_disk:
-        aux = path.join(results_file, "index_training_data.txt")
+        aux = path.join(results_file, "index_training_data_" + target + ".txt")
         np.savetxt(aux, index_training_data, fmt="%d")
         # aux = path.join(results_file, "index_training_data")
         # feather.write_dataframe(pd.DataFrame(index_training_data), aux)
 
     if "index - test data" in objects_to_save and save_objects_to_disk:
-        aux = path.join(results_file, "index_test_data.txt")
+        aux = path.join(results_file, "index_test_data_" + target + ".txt")
         np.savetxt(aux, index_test_data, fmt="%d")
         # aux = path.join(results_file, "index_test_data")
         # feather.write_dataframe(pd.DataFrame(index_test_data), aux)
 
     if "bar plot" in objects_to_save:
-        aux = path.join(results_file, "p_compare_models_bar_" + metric + ".png")
+        aux = path.join(results_file, "p_compare_models_bar_" + metric + "_" + target + ".png")
         p_compare_models_bar.figure.savefig(aux)
 
     # db = mysql.connector.connect(option_files="my.conf", use_pure=True)
