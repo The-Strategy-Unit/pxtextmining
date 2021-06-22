145ea89961c1c68edff18d89509035920b4731be
Andreas <andreassot10@gmail.com>
2021-06-22 09:45:36 +0100

    code: Chance column_names arguments.

--- pxtextmining/factories/factory_predict_unlabelled_text.py
+++ pxtextmining/factories/factory_predict_unlabelled_text.py
@@ -4,7 +4,7 @@ from itertools import chain
 
 
 def factory_predict_unlabelled_text(dataset, predictor, pipe_path,
-                                    preds_column=None, column_names='__all__'):
+                                    preds_column=None, column_names='all_cols'):
     """
     Predict unlabelled text data using a fitted `sklearn.pipeline.Pipeline
     <https://scikit-learn.org/stable/modules/generated/sklearn.pipeline.Pipeline.html>`_/`imblearn.pipeline.Pipeline
@@ -17,9 +17,9 @@ def factory_predict_unlabelled_text(dataset, predictor, pipe_path,
         the SAV file with the fitted ``Scikit-learn``/``imblearn.pipeline.Pipeline``.
     :param str preds_column: The user-specified name of the column that will have the predictions. If ``None`` (default),
         then the name will be ``text_col_name + '_preds'``.
-    :param column_names:  A vector of strings with the names of the columns of the supplied data frame (incl.
-        ``text_col_name``) to be added to the returned ``pandas.DataFrame``.  If ``None``, then the only column in the
-        returned data frame will be ``preds_column``. Defaults to "__all__".
+    :param column_names:  A ``list``/``tuple`` of strings with the names of the columns of the supplied data frame (incl.
+        ``text_col_name``) to be added to the returned ``pandas.DataFrame``.  If "preds_only", then the only column in
+        the returned data frame will be ``preds_column``. Defaults to "all_cols".
     :return: A ``pandas.DataFrame`` with the predictions and any other columns supplied in ``column_names``.
     """
 
@@ -35,16 +35,19 @@ def factory_predict_unlabelled_text(dataset, predictor, pipe_path,
     if preds_column is None:
         preds_column = predictor + '_preds'
     data_unlabelled[preds_column] = predictions
-
     data_unlabelled = data_unlabelled.rename(columns={'predictor': predictor})
 
     # Set column names of columns to return in final data frame
-    if column_names is None:
-        column_names = [predictor]
-    elif column_names == '__all__':
-        column_names == list(data_unlabelled)
+    if column_names == 'all_cols':
+        column_names = [data_unlabelled]
+    elif column_names == 'preds_only':
+        column_names = None
+    elif type(column_names) is str:
+        column_names = [column_names]
+
     returned_cols = [[preds_column], column_names] # column_names is a list. Put preds_column in a list to create a list
                                                    # of lists to unnest later to get a list of strings.
-    returned_cols = list(chain.from_iterable(returned_cols)) # Unnest list of lists.
+    returned_cols = [x for x in returned_cols if x is not None]
+    returned_cols = list(chain.from_iterable(returned_cols))  # Unnest list of lists.
 
     return data_unlabelled[returned_cols]
