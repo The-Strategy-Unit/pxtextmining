7a7d2b56b29aca1abbb2d414e567824e2b7db463
Andreas <andreassot10@gmail.com>
2021-07-14 14:54:09 +0100

    doc: Update Sphinx documentation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, NHS England'
 author = 'Andreas D Soteriades'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.3'
+release = '0.2.6'
 
 
 # -- General configuration ---------------------------------------------------
