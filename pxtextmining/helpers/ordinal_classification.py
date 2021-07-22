80aff823c219a739f0f183bc6023e5e241f14717
Andreas <andreassot10@gmail.com>
2021-07-22 15:24:18 +0100

    code: Ordinal model forcing crit = 3 for Couldn't be improved.

--- pxtextmining/helpers/ordinal_classification.py
+++ pxtextmining/helpers/ordinal_classification.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import scipy
 from sklearn.base import clone
 from sklearn.base import BaseEstimator
 from sklearn.linear_model import LogisticRegression
@@ -14,17 +15,28 @@ class OrdinalClassifier(BaseEstimator):
     <https://towardsdatascience.com/simple-trick-to-train-an-ordinal-regression-with-any-classifier-6911183d2a3c>`_.
     """
 
-    def __init__(self, estimator=LogisticRegression(), clfs={}, y_factorized=None, unique_class=None, class_dict=None):
+    def __init__(self, estimator=LogisticRegression(), clfs={}, y_factorized=None, unique_class=None, class_dict=None,
+                 theme=None, target_class_value='3', theme_class_value=1):
         self.estimator = estimator
         self.clfs = clfs
         self.y_factorized = y_factorized
         self.unique_class = unique_class
         self.class_dict = class_dict
+        self.theme = theme
+        self.target_class_value = target_class_value
+        self.theme_class_value = theme_class_value
 
     def fit(self, X, y=None, **kwargs):
         self.y_factorized = pd.Series(y.astype('int64')).factorize(sort=True)[0]
         self.unique_class = np.sort(np.unique(self.y_factorized))
         self.class_dict = dict(zip(self.y_factorized, y))
+
+        if self.theme is not None:
+            if isinstance(X, scipy.sparse.csr.csr_matrix):
+                X = pd.DataFrame(X.toarray())
+                X = pd.DataFrame(X.drop([0], axis=1))
+            # X[self.theme] = 0
+            # X = pd.DataFrame(X.drop([self.theme], axis=1))
 
         if self.unique_class.shape[0] > 2:
             for i in range(self.unique_class.shape[0] - 1):
@@ -36,6 +48,13 @@ class OrdinalClassifier(BaseEstimator):
         return self
 
     def predict_proba_all(self, X):
+        if self.theme is not None:
+            if isinstance(X, scipy.sparse.csr.csr_matrix):
+                X = pd.DataFrame(X.toarray())
+                X = pd.DataFrame(X.drop([0], axis=1))
+            # X[self.theme] = 0
+            # X = pd.DataFrame(X.drop([self.theme], axis=1))
+
         clfs_predict = {k: self.clfs[k].predict_proba(X) for k in self.clfs}
         predicted = []
 
@@ -53,15 +72,44 @@ class OrdinalClassifier(BaseEstimator):
         return np.vstack(predicted).T
 
     def predict_proba(self, X):
+        if self.theme is not None:
+            if isinstance(X, scipy.sparse.csr.csr_matrix):
+                X = pd.DataFrame(X.toarray())
+                X = pd.DataFrame(X.drop([0], axis=1))
+            # X[self.theme] = 0
+            # X = pd.DataFrame(X.drop([self.theme], axis=1))
+
         return np.max(self.predict_proba_all(X), axis=1)
 
     def predict(self, X):
+        if self.theme is not None:
+            if isinstance(X, scipy.sparse.csr.csr_matrix):
+                X = pd.DataFrame(X.toarray())
+                theme_col = X[[0]].copy()
+                X = pd.DataFrame(X.drop([0], axis=1))
+            # X[self.theme] = 0
+            # X = pd.DataFrame(X.drop([self.theme], axis=1))
+
         y_pred = np.argmax(self.predict_proba_all(X), axis=1)
         y_pred_orig_class_names = []
         for i in y_pred:
             y_pred_orig_class_names.append(self.class_dict[i])
-        return np.array(y_pred_orig_class_names)
+        re = np.array(y_pred_orig_class_names)
+
+        if self.theme is not None:
+            no_improvements_index = theme_col.loc[theme_col.iloc[:, 0] == self.theme_class_value].index
+            re = pd.DataFrame(re, columns=['aux'], index=theme_col.index)
+            re.loc[no_improvements_index] = self.target_class_value
+            re = np.array(re.aux)
+
+        return re
 
     def score(self, X, y):
+        if self.theme is not None:
+            if isinstance(X, scipy.sparse.csr.csr_matrix):
+                X = pd.DataFrame(X.toarray())
+                X = pd.DataFrame(X.drop([0], axis=1))
+            # X[self.theme] = 0
+            # X = pd.DataFrame(X.drop([self.theme], axis=1))
         return self.estimator.score(X, y)
 
