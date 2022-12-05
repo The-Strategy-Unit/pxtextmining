a22ff0a3ed140ce2909b468554b48163dc031abf
YiWen Hon <yiwench@gmail.com>
2022-12-05 14:38:30 +0000

    wrote scripts to autopopulate docs/reference based on files in pxtextmining package

---
+++ docs/create_docs.py
@@ -0,0 +1,22 @@
+import glob
+import os
+
+"""
+Python script to automatically generate .md files in docs/reference based on
+contents of pxtextmining folders
+"""
+
+
+modules = glob.glob('pxtextmining/*/')
+module_names = []
+for folder in modules:
+    if '__' not in folder:
+        module_name = folder.split('/')[-2]
+        print(f'MODULE: {module_name}')
+        pylist = glob.glob(f"{folder}/*.py")
+        for py in pylist:
+            if '__' not in py:
+                py_name = os.path.basename(py)[:-3]
+                print(py_name)
+                with open(f'docs/reference/{module_name}/{py_name}.md', 'w') as f:
+                    f.write(f'::: pxtextmining.{module_name}.{py_name}')
