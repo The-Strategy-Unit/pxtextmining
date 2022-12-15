f32ffa5e392c358cca83a86fdc9ee74c55c82244
YiWen Hon <yiwench@gmail.com>
2022-12-15 15:25:33 +0000

    got basic API working. need to work on documentation next

--- api/api.py
+++ api/api.py
@@ -14,15 +14,17 @@ def index():
 @app.get('/predict_from_sql')
 def predict(ids: str,
             target: str):
-    if target == 'label':
-        model = 'results_label/pipeline_label.sav'
-    elif target == 'criticality':
-        model = 'test_results_criticality/test_pipeline_criticality.sav'
-    else:
-        return {'error': 'invalid target'}
     q = ids.split(',')
     placeholders= ', '.join(['%s']*len(q))  # "%s, %s, %s, ... %s"
-    query = "SELECT id , feedback FROM text_data WHERE id IN ({})".format(placeholders)
+    if target == 'label':
+        model = 'results_label/pipeline_label.sav'
+        query = "SELECT id , feedback FROM text_data WHERE id IN ({})".format(placeholders)
+    elif target == 'criticality':
+        model = 'results_criticality_with_theme/pipeline_criticality_with_theme.sav'
+        query = "SELECT id , label , feedback FROM text_data WHERE id IN ({})".format(placeholders)
+    else:
+        return {'error': 'invalid target'}
+
     db = mysql.connector.connect(option_files="my.conf", use_pure=True)
     with db.cursor() as cursor:
         cursor.execute(query, tuple(q))
@@ -33,9 +35,8 @@ def predict(ids: str,
         predictions = factory_predict_unlabelled_text(dataset=text_data, predictor="feedback",
                                     pipe_path_or_object=model, columns_to_return=['id', 'predictions'])
     elif target == 'criticality':
-        # text_data = text_data.rename(columns = {'feedback': 'predictor'})
-        # data_processed = process_data(text_data)
-        # predictions = factory_predict_unlabelled_text(dataset=data_processed, predictor="predictor",
-        #                             pipe_path_or_object=model)
-        return {'error': 'not complete'}
+        text_data = text_data.rename(columns = {'feedback': 'predictor'})
+        predictions = factory_predict_unlabelled_text(dataset=text_data, predictor="predictor",
+                                                    theme = 'label', pipe_path_or_object=model,
+                                                    columns_to_return=['id', 'predictions'])
     return predictions.to_dict(orient='records')
