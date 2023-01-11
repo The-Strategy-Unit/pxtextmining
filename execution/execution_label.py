da6b1c4d66e99a9a435d13cd41c043c6ab923e46
YiWen Hon <yiwench@gmail.com>
2023-01-11 16:41:18 +0000

    removed save_pipeline_as from execution_ scripts

--- execution/execution_label.py
+++ execution/execution_label.py
@@ -36,7 +36,6 @@ pipe, tuning_results, pred, accuracy_per_class, p_compare_models_bar, index_trai
                                  ],
                                  save_objects_to_server=False,
                                  save_objects_to_disk=True,
-                                 save_pipeline_as="pipeline_label",
                                  results_folder_name="results_label",
                                  reduce_criticality=False,
                                  theme=None)
