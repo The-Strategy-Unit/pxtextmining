df594dd3ebff625f7d70595738cb1766aa0fa8f0
YiWen Hon <yiwench@gmail.com>
2023-01-18 14:51:05 +0000

    wrote a test_json endpoint which works ok! now to turn received json into df which can be fed into model for predictions

--- api/test_api.py
+++ api/test_api.py
@@ -7,7 +7,26 @@ Then you can run this test_api script to check if the API is behaving as it shou
 """
 
 
-ids = ','.join([str(x) for x in range(1,500)])
-params={"ids": ids, "target": "criticality"}
-response = requests.get("http://127.0.0.1:8000/predict_from_sql", params=params)
-print(response.json())
+def test_sql():
+    ids = ','.join([str(x) for x in range(1,50)])
+    params={"ids": ids, "target": "criticality"}
+    response = requests.get("http://127.0.0.1:8000/predict_from_sql", params=params)
+    print(response.json())
+
+def test_json():
+    json = [{'id': '1', 'feedback': 'What is this all about'},
+            {'id': '2', 'feedback': 'Everything about the service was amazing'},
+            {'id': '3', 'feedback': 'Food was absolutely disgusting'},
+            {'id': '4', 'feedback': 'Everyone is really nice'},
+            {'id': '5', 'feedback': 'Loved riding on the rollercoasters'},
+            {'id': '6', 'feedback': 'Not sure what to say in this comment'},
+            {'id': '7', 'feedback': 'Once upon a time there was an amazing comment'},
+            {'id': '8', 'feedback': 'maybe the hospital could consider hiring more horses'},
+            {'id': '9', 'feedback': 'Why were all the staff wearing hats'}
+            ]
+    response = requests.post("http://127.0.0.1:8000/test_json", json=json)
+    print(response.status_code)
+    print(response.json())
+
+if __name__ == "__main__":
+    test_json()
