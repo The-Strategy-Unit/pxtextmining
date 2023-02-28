eb2d18143a2584f53148a07467331dec09418b33
YiWen Hon <yiwench@gmail.com>
2023-02-28 16:40:37 +0000

    completed basic multilabel api

--- api/test_api.py
+++ api/test_api.py
@@ -20,16 +20,16 @@ def test_json(json):
     print(response.json())
 
 def test_json_predictions(json):
-    response = requests.post("http://127.0.0.1:8000/predict_from_json", json=json)
+    response = requests.post("http://127.0.0.1:8000/predict_multilabel", json=json)
     print(response.status_code)
     print(response.json())
 
 if __name__ == "__main__":
     df = pd.read_csv('datasets/text_data.csv')
-    df = df.loc[50:100][['feedback']]
+    df = df.loc[101:200][['feedback']]
     js = []
     for i in range(df.shape[0]):
-        js.append({'id': str(i), 'comment_text': df.iloc[i]['feedback']})
+        js.append({'comment_id': str(i), 'comment_text': df.iloc[i]['feedback']})
     print('The JSON that was sent looks like:')
     print(js[:5])
     print('The JSON that is returned is:')
