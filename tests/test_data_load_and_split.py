d037a0aa0a73217304b1a082b716f9218f573e70
YiWen Hon <yiwench@gmail.com>
2023-03-09 14:18:45 +0000

    added test for onehot encoding column in preprocessing step (to feed categorical features into TF models)

--- tests/test_data_load_and_split.py
+++ tests/test_data_load_and_split.py
@@ -7,3 +7,8 @@ def test_clean_empty_features():
     df_with_empty_lines = pd.DataFrame({'text': ['Some text', '', ' ', 'More text']})
     clean_df = factory_data_load_and_split.clean_empty_features(df_with_empty_lines)
     assert clean_df.shape == (2,1)
+
+def test_onehot():
+    df_to_onehot = pd.DataFrame({'Categories': ['A', 'B', 'C', 'A', 'A', 'B']})
+    df_onehotted = factory_data_load_and_split.onehot(df_to_onehot, 'Categories')
+    assert df_onehotted.shape == (6,3)
