3cee9cca32989dea5df542de1230c8052e4d3e93
YiWen Hon <yiwench@gmail.com>
2023-03-14 16:29:30 +0000

    sorted imports and linted write_results

--- pxtextmining/factories/factory_write_results.py
+++ pxtextmining/factories/factory_write_results.py
@@ -1,20 +1,22 @@
-from os import path
 import pickle
-from tensorflow.keras import Sequential, Model
+from os import path
+
+from tensorflow.keras import Model, Sequential
+
 
 def write_multilabel_models_and_metrics(models, model_metrics, path, dummy=False):
     for i in range(len(models)):
-        model_name = f'model_{i}'
+        model_name = f"model_{i}"
         if isinstance(models[i], (Sequential, Model)):
-            models[i].save(f'{path}/{model_name}')
+            models[i].save(f"{path}/{model_name}")
         else:
-            pickle.dump(models[i], open(f'{path}/{model_name}.sav', 'wb'))
-        with open(f'{path}/{model_name}.txt', 'w') as file:
+            pickle.dump(models[i], open(f"{path}/{model_name}.sav", "wb"))
+        with open(f"{path}/{model_name}.txt", "w") as file:
             file.write(model_metrics[i])
     if len(model_metrics) != len(models):
-        with open(f'{path}/dummy_metrics.txt', 'w') as file:
+        with open(f"{path}/dummy_metrics.txt", "w") as file:
             file.write(model_metrics[-1])
-    print(f'{len(models)} models have been written to {path}')
+    print(f"{len(models)} models have been written to {path}")
 
 
 def write_model_summary(results_file, model_summary):
@@ -25,6 +27,6 @@ def write_model_summary(results_file, model_summary):
         model_summary (dict): Model metadata to be written.
     """
     model_summary_file = path.join(results_file, "model_summary.txt")
-    with open(model_summary_file, 'w') as f:
+    with open(model_summary_file, "w") as f:
         for k, v in model_summary.items():
-            f.write(f'{k}: \n {v} \n\n')
+            f.write(f"{k}: \n {v} \n\n")
