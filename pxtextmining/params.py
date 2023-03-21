ad1eab1212d3ca255b402a38bb4e6632bae2e7f1
YiWen Hon <yiwench@gmail.com>
2023-03-21 16:51:12 +0000

    added params.py for variables that are consistent throughout packa, this should help deal with the unfortunate situation where model was trained with distilbert-uncased and I tried to use tokenizer from distilbert-cased

---
+++ pxtextmining/params.py
@@ -0,0 +1,15 @@
+major_cats = [
+        "Access to medical care & support",
+        "Activities",
+        "Additional",
+        "Category TBC",
+        "Communication & involvement",
+        "Environment & equipment",
+        "Food & diet",
+        "General",
+        "Medication",
+        "Mental Health specifics",
+        "Patient journey & service coordination",
+        "Service location, travel & transport",
+        "Staff",
+    ]
