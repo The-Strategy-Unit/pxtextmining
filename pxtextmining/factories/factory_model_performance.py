5cf47cf735a19dcd05379eff0e37efc98391cae4
YiWen Hon <yiwench@gmail.com>
2023-03-23 17:29:45 +0000

    model_performance added docstrings. need to write test for get_multilabel_metric as it has been altered. Dummy model has been taken out into separate function. Would be good to get accuracy_per_class into multilabel_metrics function

--- pxtextmining/factories/factory_model_performance.py
+++ pxtextmining/factories/factory_model_performance.py
@@ -12,29 +12,54 @@ from pxtextmining.factories.factory_predict_unlabelled_text import (
 )
 
 
+def get_dummy_model(x_train, y_train):
+    """Creates dummy model that randomly predicts labels, fitted on the training data.
+
+    Args:
+        x_train (pd.DataFrame): Input features.
+        y_train (pd.DataFrame): Target values.
+
+    Returns:
+        (sklearn.dummy.DummyClassifier): Trained dummy classifier.
+    """
+    model = DummyClassifier(strategy="uniform")
+    model.fit(x_train, y_train)
+    return model
+
 def get_multilabel_metrics(
     x_test,
     y_test,
     labels,
     random_state,
-    model_type=None,
-    model=None,
+    model_type,
+    model,
     training_time=None,
-    x_train=None,
-    y_train=None,
     additional_features=False,
     already_encoded=False,
 ):
-    """Function to produce performance metrics for a multilabel machine learning model.
+    """Creates a string detailing various performance metrics for a multilabel model, which can then be written to
+    a text file.
 
-    :param pd.DataFrame x_test: Test data (predictor).
-    :param pd.Series y_test: Test data (target).
-    :param pd.DataFrame x_train: Training data (predictor). Defaults to None, only needed if model = None
-    :param pd.Series y_train: Training data (target). Defaults to None, only needed if model = None
-    :param str model: Trained classifier. Defaults to 'dummy' which instantiates dummy classifier for baseline metrics.
+    Args:
+        x_test (pd.DataFrame): DataFrame containing test dataset features
+        y_test (pd.DataFrame): DataFrame containing test dataset true target values
+        labels (list): List containing the target labels
+        random_state (int): Seed used to control the shuffling of the data, to enable reproducible
+        results.
+        model_type (str): Type of model used. Options are 'bert', 'tf', or 'sklearn'.
+        Defaults to None.
+        model (tf.keras or sklearn model): Trained estimator.
+        training_time (str, optional): Amount of time taken for model to train. Defaults to None.
+        additional_features (bool, optional): Whether or not additional features (e.g. question type)
+        have been included in training the model. Defaults to False.
+        already_encoded (bool, optional): Whether or not, if a `bert` model was used, x_test has already been encoded.
+        Defaults to False.
 
-    :return: None
-    :rtype: None
+    Raises:
+        ValueError: Only model_type 'bert', 'tf' or 'sklearn' are allowed.
+
+    Returns:
+        (str): String containing the model architecture/hyperparameters, random state used for the train test split, and performance metrics including: exact accuracy, hamming loss, macro jaccard score, and classification report.
     """
 
     metrics_string = "\n *****************"
@@ -42,12 +67,6 @@ def get_multilabel_metrics(
         f"\n Random state seed for train test split is: {random_state} \n\n"
     )
     model_metrics = {}
-    if model == None:
-        model = DummyClassifier(strategy="uniform")
-        if isinstance(x_train, pd.Series):
-            model.fit(x_train, y_train)
-        else:
-            raise ValueError("For dummy model, x_train and y_train must be provided")
     # TF Keras models output probabilities with model.predict, whilst sklearn models output binary outcomes
     # Get them both to output the same (binary outcomes) and take max prob as label if no labels predicted at all
     if model_type in ("bert", "tf"):
@@ -58,23 +77,24 @@ def get_multilabel_metrics(
                 additional_features=additional_features,
                 already_encoded=already_encoded,
             )
-        else:
+        elif model_type == "tf":
             y_probs = model.predict(x_test)
         binary_preds = turn_probs_into_binary(y_probs)
         y_pred = fix_no_labels(binary_preds, y_probs, model_type="tf")
-    else:
+    elif model_type == "sklearn":
+        y_probs = model.predict(x_test)
         binary_preds = model.predict(x_test)
         y_probs = np.array(model.predict_proba(x_test))
         y_pred = fix_no_labels(binary_preds, y_probs, model_type="sklearn")
-    c_report_str = metrics.classification_report(
-        y_test, y_pred, target_names=labels, zero_division=0
-    )
+    else:
+        raise ValueError('Please select valid model_type. Options are "bert", "tf" or "sklearn"')
+    # Calculate various metrics
     model_metrics["exact_accuracy"] = metrics.accuracy_score(y_test, y_pred)
     model_metrics["hamming_loss"] = metrics.hamming_loss(y_test, y_pred)
     model_metrics["macro_jaccard_score"] = metrics.jaccard_score(
         y_test, y_pred, average="macro"
     )
-    if isinstance(model, (Sequential, Model)):
+    if model_type in ("bert", "tf"):
         stringlist = []
         model.summary(print_fn=lambda x: stringlist.append(x))
         model_summary = "\n".join(stringlist)
@@ -85,11 +105,10 @@ def get_multilabel_metrics(
     for k, v in model_metrics.items():
         metrics_string += f"\n{k}: {v}"
     metrics_string += "\n\n Classification report:\n"
+    c_report_str = metrics.classification_report(
+        y_test, y_pred, target_names=labels, zero_division=0
+    )
     metrics_string += c_report_str
-    # per_class_jaccard = zip(labels,metrics.jaccard_score(y_test, y_pred, average = None, zero_division = 0))
-    # metrics_string += '\nper class Jaccard score:'
-    # for k,v in per_class_jaccard:
-    #     print(f'{k}: {v}')
     return metrics_string
 
 
