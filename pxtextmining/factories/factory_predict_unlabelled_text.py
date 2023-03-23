f0d7f9ce7223ad8b52c203c77df59146aa82cec2
YiWen Hon <yiwench@gmail.com>
2023-03-23 12:15:20 +0000

    adding documentation to predict_unlabelled_text

--- pxtextmining/factories/factory_predict_unlabelled_text.py
+++ pxtextmining/factories/factory_predict_unlabelled_text.py
@@ -5,27 +5,27 @@ from pxtextmining.factories.factory_data_load_and_split import (
     bert_data_to_dataset,
     remove_punc_and_nums,
 )
+from pxtextmining.params import major_cats
 
 
 def predict_multilabel_sklearn(
     text: pd.Series,
     model,
-    labels=[
-        "Access to medical care & support",
-        "Activities",
-        "Additional",
-        "Category TBC",
-        "Communication & involvement",
-        "Environment & equipment",
-        "Food & diet",
-        "General",
-        "Medication",
-        "Mental Health specifics",
-        "Patient journey & service coordination",
-        "Service location, travel & transport",
-        "Staff",
-    ],
+    labels=major_cats,
 ):
+    """Conducts basic preprocessing to remove punctuation and numbers.
+    Utilises a pretrained sklearn machine learning model to make multilabel predictions on the cleaned text.
+    Also takes the class with the highest predicted probability as the predicted class in cases where no class has
+    been predicted. Currently used in API, cannot take additional features. Superceded by use of pipeline.
+
+    Args:
+        text (pd.Series): Series containing text data to be processed and utilised for predictions.
+        model (sklearn.base): Trained sklearn estimator able to perform multilabel classification.
+        labels (list, optional): List containing target labels. Defaults to major_cats.
+
+    Returns:
+        pd.DataFrame: DataFrame containing the labels and predictions.
+    """
     text_no_whitespace = text.replace(r"^\s*$", np.nan, regex=True)
     text_no_nans = text_no_whitespace.dropna()
     text_cleaned = text_no_nans.astype(str).apply(remove_punc_and_nums)
@@ -38,6 +38,16 @@ def predict_multilabel_sklearn(
 
 
 def get_labels(row, labels):
+    """Given a one-hot encoded row of predictions from a dataframe,
+    returns a list containing the actual predicted labels as a `str`.
+
+    Args:
+        row (pd.DataFrame): Row in a DataFrame containing one-hot encoded predicted labels.
+        labels (list): List containing all the target labels, which should also be columns in the dataframe.
+
+    Returns:
+        list: List of the labels that have been predicted for the given text.
+    """
     label_list = []
     for c in labels:
         if row[c] == 1:
@@ -48,6 +58,18 @@ def get_labels(row, labels):
 def predict_with_bert(
     data, model, max_length=150, additional_features=False, already_encoded=False
 ):
+    """_summary_
+
+    Args:
+        data (_type_): _description_
+        model (_type_): _description_
+        max_length (int, optional): _description_. Defaults to 150.
+        additional_features (bool, optional): _description_. Defaults to False.
+        already_encoded (bool, optional): _description_. Defaults to False.
+
+    Returns:
+        _type_: _description_
+    """
     if already_encoded == False:
         encoded_dataset = bert_data_to_dataset(
             data, Y=None, max_length=max_length, additional_features=additional_features
