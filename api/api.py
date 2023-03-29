6557a3bdee2d349998525b7528afb212f2d34fdc
YiWen Hon <yiwench@gmail.com>
2023-03-29 14:42:04 +0100

    added __init__ to api folder so it is treated as module.

--- api/api.py
+++ api/api.py
@@ -21,21 +21,16 @@ def index():
 
 @app.post('/predict_multilabel')
 def predict(items: List[ItemIn]):
-    print('loading model')
     with open('current_best_multilabel/svc_text_only.sav', 'rb') as model:
         loaded_model = pickle.load(model)
-    print('processing df')
     df = pd.DataFrame([i.dict() for i in items], dtype=str)
     df_newindex = df.set_index('comment_id')
     df_newindex.index.rename('Index', inplace = True)
     text_to_predict = df_newindex['comment_text']
-    print('making predictions')
     preds_df = predict_multilabel_sklearn(text_to_predict, loaded_model)
     preds_df['comment_id'] = preds_df.index.astype(str)
-    print('merging predictions with input')
     merged = pd.merge(df, preds_df, how='inner', on='comment_id')
     return_dict = {'comments_labelled': merged[['comment_id', 'comment_text', 'labels']].to_dict(orient='records')}
-    print('not_processed step')
-    not_processed = [i for i in df['comment_id'] if i not in preds_df['comment_id']]
-    return_dict['comment_ids_failed'] = not_processed
+    comments_lost = [i for i in df['comment_id'] if i not in preds_df['comment_id']]
+    return_dict['comment_ids_failed'] = comments_lost
     return return_dict
