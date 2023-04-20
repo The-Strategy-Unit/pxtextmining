979de30fe03fdb54a3dd5a16045583fef488ba9c
YiWen Hon <yiwench@gmail.com>
2023-04-20 16:01:14 +0100

    added q_type to api/test_api

--- api/test_api.py
+++ api/test_api.py
@@ -13,15 +13,24 @@ def test_json_predictions(json):
     response = requests.post("http://127.0.0.1:8000/predict_multilabel", json=json)
     return response
 
+
 if __name__ == "__main__":
-    df = pd.read_csv('datasets/hidden/API_test.csv')
-    df = df[['row_id', 'comment_txt']].copy().set_index('row_id')[:20]
+    df = pd.read_csv("datasets/hidden/API_test.csv")
+    df = df[["row_id", "comment_txt"]].copy().set_index("row_id")[:20]
     js = []
     for i in df.index:
-        js.append({'comment_id': str(i), 'comment_text': df.loc[i]['comment_txt']})
-    print('The JSON that was sent looks like:')
+        js.append(
+            {
+                "comment_id": str(i),
+                "comment_text": df.loc[i]["comment_txt"],
+                "question_type": "nonspecific",
+            }
+        )
+    print("The JSON that was sent looks like:")
     print(js[:5])
-    print('The JSON that is returned is:')
+    print("The JSON that was sent looks like:")
+    print(js[:5])
+    print("The JSON that is returned is:")
     returned_json = test_json_predictions(js).json()
     print(returned_json)
     # json_object = json.dumps(returned_json, indent=4)
