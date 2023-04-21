4f0afa4c8951e4eae4a97f31a8879f16156e7496
YiWen Hon <yiwench@gmail.com>
2023-04-21 11:29:03 +0100

    formatted with black

--- api/api.py
+++ api/api.py
@@ -6,13 +6,15 @@ import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel, validator
 
-from pxtextmining.factories.factory_predict_unlabelled_text import \
-    predict_multilabel_sklearn
+from pxtextmining.factories.factory_predict_unlabelled_text import (
+    predict_multilabel_sklearn,
+)
 
 description = """
 This API is for classifying patient experience qualitative data,
 utilising the models trained as part of the pxtextmining project.
 """
+
 
 class ItemIn(BaseModel):
     comment_id: str
@@ -24,15 +26,18 @@ class ItemIn(BaseModel):
             "example": {
                 "comment_id": "01",
                 "comment_text": "Nurses were friendly. Parking was awful.",
-                "question_type": "nonspecific"
+                "question_type": "nonspecific",
             }
         }
 
-    @validator('question_type')
+    @validator("question_type")
     def question_type_validation(cls, v):
-        if v not in ['what_good', 'could_improve', 'nonspecific']:
-            raise ValueError('question_type must be one of what_good, could_improve, or nonspecific')
+        if v not in ["what_good", "could_improve", "nonspecific"]:
+            raise ValueError(
+                "question_type must be one of what_good, could_improve, or nonspecific"
+            )
         return v
+
 
 class ItemOut(BaseModel):
     comment_id: str
@@ -44,9 +49,10 @@ class ItemOut(BaseModel):
             "example": {
                 "comment_id": "01",
                 "comment_text": "Nurses were friendly. Parking was awful.",
-                "labels": ['Staff manner & personal attributes', 'Parking']
+                "labels": ["Staff manner & personal attributes", "Parking"],
             }
         }
+
 
 app = FastAPI(
     title="pxtextmining API",
@@ -60,8 +66,8 @@ app = FastAPI(
     license_info={
         "name": "MIT License",
         "url": "https://github.com/CDU-data-science-team/pxtextmining/blob/main/LICENSE",
-    }
-    )
+    },
+)
 
 
 @app.get("/")
@@ -76,8 +82,8 @@ def predict(items: List[ItemIn]):
     Args:
         items (List[ItemIn]): JSON list of dictionaries with the following compulsory keys:
         - `comment_id` (str)
-        - `comment_text` (str), and
-        - `question_type` (str).
+        - `comment_text` (str)
+        - `question_type` (str)
         The 'question_type' must be one of three values: 'nonspecific', 'what_good', and 'could_improve'.
         For example, `[{'comment_id': '1', 'comment_text': 'Thank you', 'question_type': 'what_good'}, {'comment_id': '2', 'comment_text': 'Food was cold', 'question_type': 'could_improve'}]`
 
@@ -89,14 +95,14 @@ def predict(items: List[ItemIn]):
     df = pd.DataFrame([i.dict() for i in items], dtype=str)
     df_newindex = df.set_index("comment_id")
     if df_newindex.index.duplicated().sum() != 0:
-        raise ValueError('comment_id must all be unique values')
+        raise ValueError("comment_id must all be unique values")
     df_newindex.index.rename("Comment ID", inplace=True)
     text_to_predict = df_newindex[["comment_text", "question_type"]]
     text_to_predict = text_to_predict.rename(
         columns={"comment_text": "FFT answer", "question_type": "FFT_q_standardised"}
     )
     # Make predictions
-    model_path = os.path.join('current_best_multilabel', 'svc_minorcats_v5.sav')
+    model_path = os.path.join("current_best_multilabel", "svc_minorcats_v5.sav")
     with open(model_path, "rb") as model:
         loaded_model = pickle.load(model)
     preds_df = predict_multilabel_sklearn(
