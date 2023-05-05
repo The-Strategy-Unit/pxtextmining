262f54ecc8ee1b0cf0933cdbd307e9b91475fed0
YiWen Hon <yiwench@gmail.com>
2023-05-05 10:46:49 +0100

    reused write_multilabel_models_and_metrics in sentiment_pipeline

--- pxtextmining/pipelines/sentiment_pipeline.py
+++ pxtextmining/pipelines/sentiment_pipeline.py
@@ -1,82 +1,51 @@
-import random
-import pandas as pd
-import os
-import pickle
 
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report
+import random
 
 from pxtextmining.factories.factory_data_load_and_split import (
-    bert_data_to_dataset,
-    load_multilabel_data,
-    process_and_split_data,
-)
-from pxtextmining.factories.factory_model_performance import (
-    get_multilabel_metrics,
-    parse_metrics_file,
-)
-from pxtextmining.factories.factory_pipeline import (
-    search_sklearn_pipelines,
-)
+    load_multilabel_data, process_and_split_data)
+from pxtextmining.factories.factory_model_performance import get_multiclass_metrics
+from pxtextmining.factories.factory_pipeline import search_sklearn_pipelines
 from pxtextmining.factories.factory_write_results import (
-    write_multilabel_models_and_metrics,
-    write_model_preds,
-)
-from pxtextmining.helpers.text_preprocessor import tf_preprocessing
+    write_multilabel_models_and_metrics)
 from pxtextmining.params import dataset
+
 
 def run_sentiment_pipeline(
     additional_features=False,
     models_to_try=["svm", "xgb"],
     path="test_multilabel/sentiment",
 ):
+    target_names = ["very positive", "positive", "neutral", "negative", "very negative"]
     random_state = random.randint(1, 999)
-    df = load_multilabel_data(filename=dataset, target='sentiment')
+    df = load_multilabel_data(filename=dataset, target="sentiment")
     X_train, X_test, Y_train, Y_test = process_and_split_data(
         df,
-        target='sentiment',
+        target="sentiment",
         additional_features=additional_features,
-        random_state=random_state
+        random_state=random_state,
     )
     models, training_times = search_sklearn_pipelines(
         X_train,
-        Y_train, target = 'sentiment',
+        Y_train,
+        target="sentiment",
         models_to_try=models_to_try,
         additional_features=additional_features,
     )
-    if not os.path.exists(path):
-        os.makedirs(path)
+    model_metrics = []
     for i in range(len(models)):
         m = models[i]
         t = training_times[i]
-        model_name = models_to_try[i]
-        modelpath = os.path.join(path, model_name + '.sav')
-        pickle.dump(m, open(modelpath, "wb"))
-        txtpath = os.path.join(path, model_name + '.txt')
-        y_preds = m.predict(X_test)
-        with open(txtpath, "w") as file:
-            metrics_string = ""
-            metadata = f"random state = {random_state} \n training time = {t}"
-            metrics_string += metadata
-            metrics_string += f"\n\n{m}\n\n"
-            report = classification_report(Y_test, y_preds,
-                                           target_names = ["very positive", "positive", "neutral", "negative", "very negative"],
-                                           zero_division = 0)
-            metrics_string += f"\n\n{report}"
-            file.write(metrics_string)
+        metrics = get_multiclass_metrics(
+            X_test,
+            Y_test,
+            labels=target_names,
+            random_state=random_state,
+            model=m,
+            training_time=t,
+        )
+        model_metrics.append(metrics)
+    write_multilabel_models_and_metrics(models, model_metrics, path)
 
-    #     model_metrics.append(
-    #         get_multilabel_metrics(
-    #             X_test,
-    #             Y_test,
-    #             random_state=random_state,
-    #             labels=target,
-    #             model_type="sklearn",
-    #             model=m,
-    #             training_time=t,
-    #         )
-    #     )
-    # write_multilabel_models_and_metrics(models, model_metrics, path=path)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run_sentiment_pipeline()
