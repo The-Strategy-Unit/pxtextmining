04939f3c3a24653e30edb9fcb1700e9591f2ac8c
YiWen Hon <yiwench@gmail.com>
2023-06-13 12:48:08 +0100

    wrote the test first ;)

--- tests/test_api.py
+++ tests/test_api.py
@@ -10,7 +10,7 @@ def test_main():
     assert response.json() == {"test": "Hello"}
 
 
-def test_predictions():
+def test_multilabel_predictions():
     test_json = [
         {"comment_id": "99999", "comment_text": "I liked all of it", 'question_type': 'nonspecific'},
         {"comment_id": "A55", "comment_text": "", 'question_type': 'nonspecific'},
@@ -20,3 +20,16 @@ def test_predictions():
     ]
     response = client.post("/predict_multilabel", json=test_json).json()
     assert len(test_json) == len(response)
+    assert type(response[0]['labels']) == list
+
+def test_sentiment_predictions():
+    test_json = [
+        {"comment_id": "99999", "comment_text": "I liked all of it", 'question_type': 'nonspecific'},
+        {"comment_id": "A55", "comment_text": "", 'question_type': 'nonspecific'},
+        {"comment_id": "A56", "comment_text": "Truly awful time finding parking", 'question_type': 'could_improve'},
+        {"comment_id": "4", "comment_text": "I really enjoyed the session", 'question_type': 'what_good'},
+        {"comment_id": "5", "comment_text": "7482367", 'question_type': 'nonspecific'},
+    ]
+    response = client.post("/predict_sentiment", json=test_json).json()
+    assert len(test_json) == len(response)
+    assert type(response[0]['sentiment']) == int
