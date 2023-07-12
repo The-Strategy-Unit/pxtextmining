4fdd06c978d475deffada108ff87ae90a512abcf
YiWen Hon <yiwench@gmail.com>
2023-07-12 16:18:13 +0100

    parametrized target for sklearn pipeline. wrote svc pipeline test

--- tests/test_multilabel_pipeline.py
+++ tests/test_multilabel_pipeline.py
@@ -1,8 +1,12 @@
 from unittest.mock import patch
 
+import pytest
+
+from pxtextmining.params import major_cats, merged_minor_cats, minor_cats
 from pxtextmining.pipelines import multilabel_pipeline
 
 
+@pytest.mark.parametrize("target", [major_cats, minor_cats, merged_minor_cats])
 @patch("pxtextmining.pipelines.multilabel_pipeline.write_model_analysis")
 @patch("pxtextmining.pipelines.multilabel_pipeline.write_model_preds")
 @patch("pxtextmining.pipelines.multilabel_pipeline.write_multilabel_models_and_metrics")
@@ -18,13 +22,49 @@ def test_sklearn_pipeline(
     mock_write,
     mock_writepreds,
     mock_writeanalysis,
+    target,
 ):
     # arrange mocks
     mock_datasplit.return_value = (1, 2, 3, 4)
     mock_skpipeline.return_value = (["model"], ["training_time"])
 
     # act
-    multilabel_pipeline.run_sklearn_pipeline(include_analysis=True)
+    multilabel_pipeline.run_sklearn_pipeline(target=target, include_analysis=True)
+
+    # assert
+    mock_dataload.assert_called_once()
+    mock_datasplit.assert_called_once()
+    mock_skpipeline.assert_called_once()
+    mock_metrics.assert_called_once()
+    mock_write.assert_called_once()
+    mock_writepreds.assert_called_once()
+    mock_writeanalysis.assert_called_once()
+
+
+@pytest.mark.parametrize("target", [major_cats, minor_cats, merged_minor_cats])
+@patch("pxtextmining.pipelines.multilabel_pipeline.write_model_analysis")
+@patch("pxtextmining.pipelines.multilabel_pipeline.write_model_preds")
+@patch("pxtextmining.pipelines.multilabel_pipeline.write_multilabel_models_and_metrics")
+@patch("pxtextmining.pipelines.multilabel_pipeline.get_multilabel_metrics")
+@patch("pxtextmining.pipelines.multilabel_pipeline.create_and_train_svc_model")
+@patch("pxtextmining.pipelines.multilabel_pipeline.process_and_split_data", create=True)
+@patch("pxtextmining.pipelines.multilabel_pipeline.load_multilabel_data")
+def test_svc_pipeline(
+    mock_dataload,
+    mock_datasplit,
+    mock_skpipeline,
+    mock_metrics,
+    mock_write,
+    mock_writepreds,
+    mock_writeanalysis,
+    target,
+):
+    # arrange mocks
+    mock_datasplit.return_value = (1, 2, 3, 4)
+    mock_skpipeline.return_value = ("model", "training_time")
+
+    # act
+    multilabel_pipeline.run_svc_pipeline(target=target, include_analysis=True)
 
     # assert
     mock_dataload.assert_called_once()
